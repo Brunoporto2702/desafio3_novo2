d55f4e777cae9b09bfad8bb2ac6573042cca19d6
lucarfacciolo <43884274+lucarfacciolo@users.noreply.github.com>
2019-11-18 19:30:31 -0300

    d

--- frontend/js/projeto_final.js
+++ frontend/js/projeto_final.js
@@ -1,17 +1,3 @@
-$(document).ready(function(){
-	$.ajax({
-		method: "GET",
-		url: "/exemplo/api/maquina",
-		//url: "http://127.0.0.1:5000/api/maquina",
-		dataType: 'json',
-	}).done(function(resp){
-		updateList(resp);
-	})
-	.fail(function(error) {
-		alert( "error" );
-  	})
-})
-
 $("#acender").click(function() {
 	$.ajax({
 		method: "GET",
